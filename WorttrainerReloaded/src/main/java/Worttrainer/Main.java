77184534d91db4bd5bdfdb40042c7eb873f7d6af
nkurzmann <93911059+nkurzmann@users.noreply.github.com>
2023-10-11 08:52:43 +0200

    Finished Projekt
    Finished Projekt, everything works, persistence, ui, ...

---
+++ WorttrainerReloaded/src/main/java/Worttrainer/Main.java
@@ -0,0 +1,30 @@
+package Worttrainer;
+
+import com.sun.javafx.scene.control.skin.VirtualFlow;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        Woerterpaare woerterpaare1 = new Woerterpaare("Hund", "https://www.planet-wissen.de/natur/haustiere/hunde/introhundewolfgjpg104~_v-HintergrundL.jpg");
+        Woerterpaare woerterpaare = new Woerterpaare("Katze", "https://www.kindernetz.de/wissen/tierlexikon/1655279778114,steckbrief-katze-102~_v-16x9@2dL_-6c42aff4e68b43c7868c3240d3ebfa29867457da.jpg");
+        Woerterpaare woerterpaare2 = new Woerterpaare("Affe", "https://image.stern.de/7561920/t/5H/v4/w1440/r1/-/affen-selfie-peta-david-slater.jpg");
+        Rechtschreibtrainer rechtschreibtrainer = new Rechtschreibtrainer();
+        rechtschreibtrainer.wortpaarhinzufuegen(woerterpaare);
+        rechtschreibtrainer.wortpaarhinzufuegen(woerterpaare1);
+        rechtschreibtrainer.wortpaarhinzufuegen(woerterpaare2);
+
+        rechtschreibtrainer.raten();
+
+        Speicherstrategie json = new Json();
+        json.save(rechtschreibtrainer);
+
+
+
+    }
+}
